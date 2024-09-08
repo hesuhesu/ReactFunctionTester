59dddcfed75652ac1b080fd3f52524a4c646eda2
hesuhesu <zndwk0728@gmail.com>
2024-09-09 02:04:20 +0900

    modified 3D Function

--- src/components/Three.js
+++ src/components/Three.js
@@ -68,6 +68,28 @@ const Three = () => {
             autoRotate = !autoRotate; // 자동 회전 상태 전환
           });
 
+          /* 수정할 부분
+          const raycaster = new THREE.Raycaster();
+          const pointer = new THREE.Vector2();
+
+          function onPointerMove( event ) {
+            // calculate pointer position in normalized device coordinates
+            // (-1 to +1) for both components
+            pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+            pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+          }
+          // update the picking ray with the camera and pointer position
+          raycaster.setFromCamera( pointer, camera );
+
+          // calculate objects intersecting the picking ray
+          const intersects = raycaster.intersectObjects( scene.children );
+
+          for ( let i = 0; i < intersects.length; i ++ ) {
+            intersects[ i ].object.material.color.set( 0x00ffff );
+          }
+          window.addEventListener( 'pointermove', onPointerMove );
+          */
+
           const clock = new THREE.Clock();
           const animate = () => {
             requestAnimationFrame(animate);
