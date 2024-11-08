c23cbdff1d85977005c93ba75bd910b517506dc9
hesuhesu <zndwk0728@gmail.com>
2024-11-09 00:42:09 +0900

    Modify Review Structure

--- src/routes/Review.js
+++ src/routes/Review.js
@@ -1,34 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import axios from 'axios';
+import Swal from "sweetalert2";
 
 import styled from "styled-components";
 import CommonTable from '../components/CommonTable';
 import CommonTableColumn from '../components/CommonTableColumn';
 import CommonTableRow from '../components/CommonTableRow';
 
+import '../css/Review.scss';
+
 const ITEMS_PER_PAGE = 10; // 페이지당 항목 수
 
-const Wrapper = styled.div`
+const ReviewDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const BoardDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 75vh;
+`
+
+const WriteDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 20vh;
+`
+
+const ButtonDiv = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 15vh;
-`;
-
-const BoxOne = styled.div`
-  background-color: #cf6a87;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: #574b90;
-  width: 100px;
-  height: 100px;
-`;
+`
 
 const HOST = process.env.REACT_APP_HOST;
 const PORT = process.env.REACT_APP_PORT;
@@ -36,13 +43,17 @@ const PORT = process.env.REACT_APP_PORT;
 const Review = () => {
 
   const [data, setData] = useState([]);
-  const [write, setWrite] = useState(false);
+  const [writeTrue, setWriteTrue] = useState(false);
+  const [writeData, setWriteData] = useState({
+    title: '',
+    content: ''
+  });
   const [pageCount, setPageCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get(`${HOST}:${PORT}/board/all_board_list`)
+    axios.get(`${HOST}:${PORT}/review/read`)
       .then((response) => {
         setData(response.data.list);
         setPageCount(Math.ceil(response.data.list.length / ITEMS_PER_PAGE)); // 총 페이지 수 계산
@@ -61,60 +72,74 @@ const Review = () => {
   const displayedData = data.slice(currentPage * ITEMS_PER_PAGE, (currentPage + 1) * ITEMS_PER_PAGE);
 
   const handleWriteReview = () => {
-    if (write){
-      axios.get(`${HOST}:${PORT}/board/all_board_list`)
-      .then((response) => {
-        setData(response.data.list);
-        setPageCount(Math.ceil(response.data.list.length / ITEMS_PER_PAGE)); // 총 페이지 수 계산
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    }
-    setWrite(!write);
+    axios.post(`${HOST}:${PORT}/review/write`, {
+      title: writeData.title,
+      content: writeData.content,
+    }).then((res) => {
+      setWriteData((prevState) => ({ ...prevState, title: '' }));
+      setWriteData((prevState) => ({ ...prevState, content: '' }));
+      Swal.fire({
+        title: "알림",
+        icon:'success',
+        html: "리뷰가 완료되었습니다.",
+        showCancelButton: false,
+        confirmButtonText: "확인",
+      }).then(() => {});
+    }).catch((e) => { console.log("error"); });
+    setWriteTrue(!writeTrue);
   }
 
   return (
-    <div>
-      <Wrapper>
-        <BoxOne />
-        <BoxTwo />
-      </Wrapper>
-      {write && <div>
-        <h2>글 작성하기</h2>
-        <input></input>
-        <textarea></textarea>
-      </div>
-      }
-      {data.length > 0 ? (
-        <>
-          <h1 className="Board-h1">Board</h1>
-          <CommonTable headersName={['제목[클릭]', '내용', '작성자', '등록일']}>
-            {displayedData.map((item) => (
-              <CommonTableRow key={item._id}>
-                <CommonTableColumn><Link to={`/board_detail/${item._id}`}>{item.title}</Link></CommonTableColumn>
-                <CommonTableColumn><Link to={`/board_detail/${item._id}`}>{item.content}</Link></CommonTableColumn>
-                <CommonTableColumn><Link to={`/board_detail/${item._id}`}>{item.writer}</Link></CommonTableColumn>
-                <CommonTableColumn><Link to={`/board_detail/${item._id}`}>{item.createdAt}</Link></CommonTableColumn>
-              </CommonTableRow>
-            ))}
-          </CommonTable>
-          <ReactPaginate
-            previousLabel={'이전'}
-            nextLabel={'다음'}
-            breakLabel={'...'}
-            pageCount={pageCount}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={5}
-            onPageChange={handlePageClick}
-            containerClassName={'pagination'}
-            activeClassName={'active'}
-          />
+    <ReviewDiv>
+      <BoardDiv>
+        <h1 className="Board-h1">Board</h1>
+        {data.length > 0 ? (
+          <>
+            <CommonTable headersName={['제목[클릭]', '내용', '작성 시기']}>
+              {displayedData.map((item) => (
+                <CommonTableRow key={item._id}>
+                  <CommonTableColumn>{item.title}</CommonTableColumn>
+                  <CommonTableColumn>{item.content}</CommonTableColumn>
+                  <CommonTableColumn>{item.createdAt}</CommonTableColumn>
+                </CommonTableRow>
+              ))}
+            </CommonTable>
+            <ReactPaginate
+              previousLabel={'이전'}
+              nextLabel={'다음'}
+              breakLabel={'...'}
+              pageCount={pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={handlePageClick}
+              containerClassName={'pagination'}
+              activeClassName={'active'}
+            />
+          </>
+        ) : <div>게시물 없음</div>}
+      </BoardDiv>
+
+      <WriteDiv>
+        {writeTrue && <>
+          <h2>리뷰 작성하기</h2>
+          제목<input onChange={(e) => setWriteData((prevState) => ({ ...prevState, title: e.target.value }))} required></input>
+          글<textarea onChange={(e) => setWriteData((prevState) => ({ ...prevState, content: e.target.value }))} required></textarea>
         </>
-      ) : <div>게시물 없음</div>}
-      <button onClick={() => navigate("/")}>홈으로 가기</button>
-      <button onClick={handleWriteReview}>{write ? '저장 완료' : '글 작성하기'}</button>
-    </div>
+        }
+      </WriteDiv>
+
+
+      <ButtonDiv>
+        <button onClick={() => navigate("/")}>홈으로 가기</button>
+        {writeTrue ? <>
+          <button onClick={handleWriteReview}>저장하기</button>
+          <button onClick={() => setWriteTrue(!writeTrue)}>취소하기</button></>
+          :
+          <button onClick={() => setWriteTrue(!writeTrue)}>글 작성하기</button>
+        }
+      </ButtonDiv>
+
+    </ReviewDiv>
   )
 }
 
